566c3dd2f562908c672a95a516e26671f4290379
elijah <eli.heineman@gmail.com>
2017-09-09 01:32:11 +0000

    added line chart with bitcoin data

--- src/MainChart.jsx
+++ src/MainChart.jsx
@@ -4,7 +4,6 @@ class MainChart extends Component {
     render() {
         return (
             <div className='mainChart'>
-                <h1>mainChart</h1>
             </div>
         );
     }
