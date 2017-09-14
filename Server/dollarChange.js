45c5a2bb5d28db3f157cbc3d7eac44fe3206e0ed
Bhav <bhavdip@gmail.com>
2017-09-14 16:25:06 +0000

    started value change file

---
+++ Server/dollarChange.js
@@ -0,0 +1,15 @@
+var request = require('request');
+const settings = require('./settings')
+var nodemailer = require('nodemailer');
+
+const knex = require('knex') ({
+  client : 'pg',
+  connection : {
+    user : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host: settings.hostname,
+    port: settings.port,
+    ssl: settings.ssl
+  }
+});
