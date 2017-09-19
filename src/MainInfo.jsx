136a0a64c10cff6e2af27f06be4b46a9cb9b3590
Vadim <vadim@gavrish.ca>
2017-09-19 22:15:27 +0000

    style

--- src/MainInfo.jsx
+++ src/MainInfo.jsx
@@ -5,14 +5,14 @@ class MainInfo extends Component {
     	const userCoinInfo = this.props.userCoinInfo
 
     	const coinList = userCoinInfo.map((coin, index) => {
-    		return <p className='tickerInfo' key={index}>{coin.coin}  |  {coin.quantity}  |  CAD ${coin.total}</p>
+    		return <p className='infoTicker' key={index}>{coin.coin}  |  {coin.quantity}  |  CAD ${coin.total}</p>
     	})
         return (
             <div className='mainInfo'>
-                <p>{this.props.userInfo.username}'s Coins</p>
-                <p>Coin | Amount | Total</p>
+                <h3 className='infoName'>Dave's Coins</h3>
+                <p className='infoTitle'>Coin | Amount | Total</p>
                 {coinList}
-                <p>-----------</p>
+                <p>_________________</p>
                 <p>Total: ${Math.round(this.props.totalCoinValue * 100) / 100}</p>
             </div>
         );
--- src/WelcomeMessage.jsx
+++ src/WelcomeMessage.jsx
@@ -4,7 +4,7 @@ class WelcomeMessage extends Component {
    render() {
        return (
            <div className='welcomeMessage'>
-               <h1>Welcome {this.props.currentUser.username}, to your personalized portfiolio</h1>
+               <h2>Welcome Dave, here are your stats for the day</h2>
            </div>
        );
    }
