11821fb4240e7c7feca0a6e49210a6c47270408e
elijah <eli.heineman@gmail.com>
2017-09-19 21:59:00 +0000

    added bitcoin logo when no pic in reddit

--- src/AreaFive.jsx
+++ src/AreaFive.jsx
@@ -7,10 +7,18 @@ class AreaFive extends Component {
     enableScroll = (e) => {
         document.body.style.overflow='auto'
     }
+    checkImg = (img) => {
+        if (img === 'self') {
+            return 'https://bitstickers.net/wp-content/uploads/2013/12/btc-mono-orange.jpg'
+        } else {
+            return img
+        }
+    }
     render() {
         const redditList = this.props.reddit.map((thread, index) => {
+            let image = this.checkImg(thread.thumbnail)
            return <a href={'https://www.reddit.com' + thread.permalink} target="_blank"><div className='reddit clearfix' key={index}>
-                    <p className='redditThread'><img src={thread.thumbnail} height='20' width='20' /><span className='redditVotes'> {thread.ups}</span><span className='divider'> | </span><a href={'https://www.reddit.com' + thread.permalink} target="_blank">{thread.title}</a></p>
+                    <p className='redditThread'><img src={image} height='20' width='20' /><span className='redditVotes'> {thread.ups}</span><span className='divider'> | </span><a href={'https://www.reddit.com' + thread.permalink} target="_blank">{thread.title}</a></p>
                   </div></a>
         })
         return (
